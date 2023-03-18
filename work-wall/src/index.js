4a271054109700eded28eb3a8757e6145bf8810c
IvoHSB <89740798+IvoHSB@users.noreply.github.com>
2023-03-18 16:33:35 +0200

    clear bugs

--- work-wall/src/index.js
+++ work-wall/src/index.js
@@ -2,14 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './styles.css';
 import reportWebVitals from './reportWebVitals';
-import { Masthead } from './components/homePage/Masthead';
-import { Navigation } from './components/navigation/Navigation';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Navigation />
-    <Masthead />
+    <App />
   </React.StrictMode>
 );
 
