5c9b59cc133735fd21aac5ff57de70af28b6ef11
IvoHSB <89740798+IvoHSB@users.noreply.github.com>
2023-03-21 11:16:01 +0200

    make check box for select skills

--- work-wall/src/components/profilePage/EditUserDatails.js
+++ work-wall/src/components/profilePage/EditUserDatails.js
@@ -1,7 +1,7 @@
 export const EditUserDetails = () => {
     return (
         <div className="container" style={{ marginTop: "170px", marginBottom: "167px" }}>
-            <h1 className="text-center my-5">Create an Account</h1>
+            <h1 className="text-center my-5">Enter details</h1>
             <div className="row justify-content-center">
                 <div className="col-md-6">
                     <form>
@@ -21,12 +21,23 @@ export const EditUserDetails = () => {
                             <label htmlFor="textareaAboutYou" className="form-label">About you</label>
                             <textarea className="form-control" id="textareaAboutYou" rows="3"></textarea>
                         </div>
+                        <div className="mb-3">
+                            <p><b>Select Your Skills</b></p> 
+                            <input type="checkbox" className="btn-check" id="btn-check-2" checked autoComplete="off" />
+                            <label className="btn btn-primary" htmlFor="btn-check-2">HTML</label>
+                            <input type="checkbox" className="btn-check" id="btn-check-2" checked autoComplete="off" />
+                            <label className="btn btn-primary" htmlFor="btn-check-2">CSS</label>
+                            <input type="checkbox" className="btn-check" id="btn-check-2" checked autoComplete="off" />
+                            <label className="btn btn-primary" htmlFor="btn-check-2">JS</label>
+                            <input type="checkbox" className="btn-check" id="btn-check-2" checked autoComplete="off" />
+                            <label className="btn btn-primary" htmlFor="btn-check-2">React</label>
+                        </div>
                         <div className="text-center">
-                            <button type="submit" className="btn btn-primary">Create Account</button>
+                            <button type="submit" className="btn btn-primary">Continued</button>
                         </div>
                     </form>
                 </div>
-            </div>
-        </div>
+            </div >
+        </div >
     );
 }
