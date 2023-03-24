53ab7ce9959863d427daaf15bc21fabc5000006b
IvoHSB <89740798+IvoHSB@users.noreply.github.com>
2023-03-24 18:59:07 +0200

    bug fix similar projects

--- work-wall/src/components/projectDetailsPage/SimilarProjects.js
+++ work-wall/src/components/projectDetailsPage/SimilarProjects.js
@@ -5,32 +5,37 @@ export const SimilarProjects = () => {
 
     let similarProjects = useSelector((state) => state.project.similarProjects);
 
-    return (<section className="page-section" id="portfolio">
-        <div className="container">
-            <h2>Similar Projects</h2>
-            <div className="row">
-                {similarProjects && similarProjects.map(simProject =>
-                    simProject ?
-                        <div key={simProject._id} className="col-lg-4 col-sm-6 mb-4">
-                            <div className="portfolio-item">
-                                <Link className="portfolio-link" data-bs-toggle="modal" to={`/projects/${simProject._id}`}>
-                                    <div className="portfolio-hover">
-                                        <div className="portfolio-hover-content"><i className="fas fa-plus fa-3x"></i></div>
+    if (similarProjects[1]) {
+        return (
+            <section className="page-section" id="portfolio">
+                <div className="container">
+                    <h2>Similar Projects</h2>
+                    <div className="row">
+                        {similarProjects && similarProjects.map(simProject =>
+                            simProject ?
+                                <div key={simProject._id} className="col-lg-4 col-sm-6 mb-4">
+                                    <div className="portfolio-item">
+                                        <Link className="portfolio-link" data-bs-toggle="modal" to={`/projects/${simProject._id}`}>
+                                            <div className="portfolio-hover">
+                                                <div className="portfolio-hover-content"><i className="fas fa-plus fa-3x"></i></div>
+                                            </div>
+                                            <img className="img-fluid" src={simProject.photo} alt="..." />
+                                        </Link>
+                                        <div className="portfolio-caption bg-light">
+                                            <div className="portfolio-caption-heading">{simProject.title}</div>
+                                            <div className="portfolio-caption-subheading text-muted">{simProject.category !== 'Other' ? simProject.category : simProject.otherCategory}</div>
+                                        </div>
                                     </div>
-                                    <img className="img-fluid" src={simProject.photo} alt="..." />
-                                </Link>
-                                <div className="portfolio-caption bg-light">
-                                    <div className="portfolio-caption-heading">{simProject.title}</div>
-                                    <div className="portfolio-caption-subheading text-muted">{simProject.category !== 'Other' ? simProject.category : simProject.otherCategory}</div>
                                 </div>
-                            </div>
-                        </div>
-                        :
-                        null
-                )}
-            </div>
-        </div>
-    </section>
-    );
+                                :
+                                null
+                        )}
+                    </div>
+                </div>
+            </section>
+        );
+    }
+
+    return null;
 
 }
