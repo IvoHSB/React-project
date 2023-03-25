de5f69869838d86217daa9bf487748409dd099ae
IvoHSB <89740798+IvoHSB@users.noreply.github.com>
2023-03-25 18:53:48 +0200

    remove unused files

--- work-wall/src/store/store.js
+++ work-wall/src/store/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './user/user';
-import profileReducer from './profilePage/profile';
+import profileReducer from './profile/profile';
 import projectReducer from './project/project'
 
 
