a42d5d79c61f4e2b780a9d901a2b9289e1b019c9
IvoHSB <89740798+IvoHSB@users.noreply.github.com>
2023-03-27 19:32:41 +0300

    fix footer

--- work-wall/src/components/404Page/404.js
+++ work-wall/src/components/404Page/404.js
@@ -14,7 +14,7 @@ export const NotFound = () => {
     }, []);
 
     return (
-        <div className="container d-flex justify-content-center align-items-center" style={{marginTop: "230px", marginBottom: "259px"}}>
+        <div className="container d-flex justify-content-center align-items-center" style={{marginTop: "330px", marginBottom: "370px"}}>
             <div className="text-center">
                 <h1 className="mb-3">Oops! Page not found</h1>
                 <p className="mb-4">The page you are looking for might have been removed, had its name changed, or is temporarily unavailable.</p>
